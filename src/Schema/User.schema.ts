db60378633a6b2754fe8b70e6b8e3be374a4d996
Sarah Hisham <sarahishamsaied@gmail.com>
2024-06-03 14:39:19 +0300

    updated username

--- src/Schema/User.schema.ts
+++ src/Schema/User.schema.ts
@@ -3,7 +3,7 @@ import Joi from "joi";
 export const userSignUpSchema = Joi.object({
   email: Joi.string().required(),
   password: Joi.string().min(3).required(),
-  username: Joi.string().alphanum().min(3).max(30).required(),
+  username: Joi.string().min(3).required(),
   image: Joi.string().uri().allow(null),
   dateOfBirth: Joi.date().less("now").allow(null),
 });
