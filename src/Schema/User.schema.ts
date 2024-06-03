908c234d8aad3312745ec9abfa6ea39381d313f8
Sarah Hisham <sarahishamsaied@gmail.com>
2024-06-03 14:36:01 +0300

    updated validation schema

--- src/Schema/User.schema.ts
+++ src/Schema/User.schema.ts
@@ -1,14 +1,14 @@
 import Joi from "joi";
 
 export const userSignUpSchema = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+  email: Joi.string().required(),
+  password: Joi.string().min(3).required(),
   username: Joi.string().alphanum().min(3).max(30).required(),
   image: Joi.string().uri().allow(null),
   dateOfBirth: Joi.date().less("now").allow(null),
 });
 
 export const userSignInSchema = Joi.object({
-  email: Joi.string().email().required(),
+  email: Joi.string().required(),
   password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
 });
